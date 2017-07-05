010962bdbe3a56eeafd6d7850bfc55f205f6f939
Fredrik Christenson <fredrik.christenson@ticnet.se>
2017-07-05 11:14:39 +0200

    again

--- frontend/mytrip-login/src/containers/__tests__/createBookingPageContainer.test.jsx
+++ frontend/mytrip-login/src/containers/__tests__/createBookingPageContainer.test.jsx
@@ -1,20 +1,28 @@
 import React from "react";
-import { shallow } from "enzyme";
-import createBookingPageContainer from "../createBookingPageContainer";
-import LoginPage from "../../components/loginPage";
+import { mount } from "enzyme";
+import CreateBookingPageContainer from "../createBookingPageContainer";
+import CreateBookingPage from "../../components/createBookingPage";
+import { Provider } from "react-redux";
+import { createStore, combineReducers } from "redux";
+import * as authUtils from "../../utils/authUtil";
+import reducers from "../../reducers";
 import * as myTripAPI from "../../services/myTripAPI";
 
 jest.mock("../../services/myTripAPI");
+jest.mock("../../utils/authUtil");
 
 describe("LoginPageContainer", () => {
   it("enhances a LoginPage component with rpc call to post the login form to", () => {
     //noinspection JSAnnotator
+    let store = createStore(combineReducers(reducers));
     myTripAPI.createBooking = jest.fn(() => new Promise(() => {}));
-    const bookingNumber = "bookingNumber";
-    const CreateBookingPageContainer = createBookingPageContainer(LoginPage);
-    const component = shallow(<CreateBookingPageContainer />);
 
-    component.find(LoginPage).props().onCreateBooking(bookingNumber);
+    const bookingNumber = "bookingNumber";
+    const component = mount(
+      <Provider store={store}><CreateBookingPageContainer /></Provider>
+    );
+
+    component.find(CreateBookingPage).props().onCreateBooking(bookingNumber);
 
     expect(myTripAPI.createBooking).toHaveBeenCalledWith(bookingNumber);
   });
--- frontend/mytrip-login/src/containers/__tests__/loginPageWithRPC.test.jsx
+++ frontend/mytrip-login/src/containers/__tests__/loginPageWithRPC.test.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { shallow, mount } from "enzyme";
+import { mount } from "enzyme";
 import LoginPageWithRPC from "../loginPageWithRPC";
 import LoginPage from "../../components/loginPage";
 import { Provider } from "react-redux";
